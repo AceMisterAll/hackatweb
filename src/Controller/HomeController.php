ed55c405b5df954322539e86dce7e69d6a5095c0
Killian <killianchaoui511@gmail.com>
2022-11-16 09:34:21 +0100

    route hackacthons

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,4 +16,62 @@ class HomeController extends AbstractController
             'messageAccueil' => 'Bienvenue',
         ]);
     }
+
+    #[Route('/Hackathons', name: 'app_Hackathons')]
+    public function ListeHackathon(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            //'controller_name' => 'HomeController',
+            'messageAccueil' => 'Bienvenue',
+        ]);
+    }
 }
+
+
+
+/*public function getLesSeries() 
+{
+
+    $req = "select * from  Hackathon";
+    $res = [...]::$monPdo->prepare($req);
+    $res->execute();
+    $LesLignes = $res->fetchAll();
+    return $LesLignes;
+}
+
+
+
+
+
+
+{% extends 'base.html.twig' %}
+
+{% block body %}
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<h1> Liste des {{lesHackathons|lengh()}} Hackathons !<h1>
+{% for LeHackathon in lesHackathons %}
+<div class="card" style="width: 18rem;">
+        <img>{{ LeHackathon.image }}</img>
+        <h3>{{ LeHackathon.titre }}</h3>
+        <p>{{ LeHackathon.resumee }}</p>
+        <p>{{ LaSLeHackathonerie.duree }}</p>
+        </div>
+    {% endfor %}
+{% endblock %}
+*/
+
+
+
+
+
+
+
+
