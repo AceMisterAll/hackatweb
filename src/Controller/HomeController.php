ba9a5881481e56f5e3aea8f3ed2da839e1229ba5
Allan Chiron <allanloannchiron@gmail.com>
2022-11-16 08:32:54 +0100

    Co-authored-by: trax0305 <trax0305@users.noreply.github.com> Co-authored-by: Killian51 <Killian51@users.noreply.github.com>

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Hackathon;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,4 +19,32 @@ class HomeController extends AbstractController
         ]);
     }
     
+    #[Route('/hackathons', name: 'app_hackathons')]
+    public function leshackathons(ManagerRegistry $doctrine): Response
+    {
+        $Repository = $doctrine->getRepository(Hackathon::class);
+        $leshackathons = $Repository->findAll();
+        dump($leshackathons);
+        return $this->render('home/leshackathons.html.twig', [
+            'lesHackathons' => $leshackathons
+        ]);
+        
+    }
+    
+
+
+    #[Route('/hackathon/{id}', name: 'app_hackathons_detail')]
+    public function lehackathons(ManagerRegistry $doctrine, $id): Response
+    {
+        $repository = $doctrine->getRepository(Hackathon::class);
+        $lehackathon = $repository->find($id);
+        dump($lehackathon);
+        return $this->render('serie/detail.html.twig' ,[
+            'lehackathons' => $lehackathon
+        ]);
+
+    }
 }
+
+
+
