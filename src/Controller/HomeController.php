c0c81bdd487c472eef00470ce4d0f67069924f67
trax0305 <a.froissart05@gmail.com>
2023-05-03 21:34:02 +0200

    ajout debut fonction inscrition

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,13 +3,25 @@
 namespace App\Controller;
 
 use App\Entity\Hackathon;
+use App\Entity\Inscription;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController
+
 {
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
@@ -38,7 +50,28 @@ class HomeController extends AbstractController
             'lehackathon' => $lehackathon,
         ]);
     }
+
+    
+    #[Route('/inscription/{idHackathon}', name: "inscription")]
+    public function inscription(Request $request, ManagerRegistry $doctrine, $idHackathon)
+{
+    // Récupérer l'utilisateur connecté
+    $user = $this->getUser();
+
+    // Récupérer le hackathon à partir de l'ID
+    $repository = $doctrine->getRepository(Hackathon::class);
+    $hackathon = $repository->find($idHackathon);
+
+    // Créer une nouvelle inscription
+    $inscription = new Inscription();
+    $inscription->setHackathon($hackathon);
+    $inscription->setUser($user);
+
+    // Enregistrer l'inscription
+    $entityManager = $Doctrine->getManager();
+    $entityManager->persist($inscription);
+    $entityManager->flush();
+
+    return new Response('Inscription réussie!');
 }
-
-
-
+}
